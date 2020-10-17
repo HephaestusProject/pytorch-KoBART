58eed3a6922498866854788b6aaf83056654fa7e
차금강 <chagmgang@gmail.com>
2020-10-17 13:48:15 +0900

    [수정] 전처리 과정 multiprocessing으로 시간 단축

--- build_dataset.py
+++ build_dataset.py
@@ -1,8 +1,10 @@
 import copy
+import collections
 import multiprocessing
 import os
 import random
 import time
+
 from argparse import ArgumentParser
 from pathlib import Path
 
@@ -12,83 +14,69 @@ import torch
 import tqdm
 from transformers import BartTokenizer
 
+Returns = collections.namedtuple('Returns', 
+        ['target_token', 'masked_token', 'length', 'index'])
 
-def get_masked_encode(encode, mask_ratio, mask_token_id):
-    arange = np.arange(len(encode) - 1)
+def map_wrapper(x):
+    return x[0](*(x[1:]))
+
+def get_single_dataset(line, tokenizer, mask_ratio, index):
+    token = tokenizer.encode(line)
+    arange = np.arange(len(token) - 2)
     np.random.shuffle(arange)
-    mask_index = arange[: int(len(encode) * mask_ratio)]
+    mask_index = arange[:int(len(arange) * mask_ratio)]
     mask_index += 1
-    masked_encode = copy.deepcopy(encode)
-    masked_encode = np.stack(masked_encode)
-    masked_encode[mask_index] = mask_token_id
-    return masked_encode
+    masked_token = copy.deepcopy(token)
+    for mask in mask_index:
+        masked_token[mask] = tokenizer.mask_token_id
+    data = Returns(
+            target_token=token,
+            masked_token=masked_token,
+            length=len(token),
+            index=index)
+    if index % 1000 == 0:
+        print(f'masking token ... {index}')
+    return data
 
-
-def make_dataset(split_lines, tokenizer, repeat, mask_ratio, path):
-    max_length = 0
+def read_txt_file(source_io, tokenizer,
+                  mask_ratio, repeat):
+    input_args = []
+    t = time.time()
+    s = time.time()
     index = 0
-    max_index = len(split_lines) * repeat
-
-    dataset_path = os.path.join(path, f"dataset.h5")
-    hf = h5py.File(dataset_path, "w")
-
-    for line in tqdm.tqdm(split_lines):
-
-        encode = tokenizer.encode(line)
-
+    for line in source_io:
+        line = line.replace("\n", "")
         for _ in range(repeat):
-            mask_encode = get_masked_encode(
-                encode=encode,
-                mask_ratio=mask_ratio,
-                mask_token_id=tokenizer.mask_token_id,
-            )
-            hf.create_dataset(f"{index}/encode", data=encode)
-            hf.create_dataset(f"{index}/mask_encode", data=mask_encode)
+            input_args.append([
+                get_single_dataset,
+                line, tokenizer,
+                mask_ratio, index])
             index += 1
+        if len(input_args) % 1000 == 0:
+            print(f"read lines ... {len(input_args)}, {time.time() - s}")
+            s = time.time()
+    print(f"read all lines ... {len(input_args)}, {time.time() - t}")
+    return input_args
 
-        max_length = max([max_length, len(encode)])
+def write_file(dataset_path, datasets):
 
-    for i in range(index):
-        hf.create_dataset(f"{i}/max_length", data=max_length)
-    hf.close()
+    hf = h5py.File(dataset_path, 'w')
+    max_length = 0
+    s = time.time()
+    for step, dataset in enumerate(datasets):
+        if max_length <= dataset.length:
+            max_length = dataset.length
+        hf.create_dataset(f'{dataset.index}/encode', data=dataset.target_token)
+        hf.create_dataset(f'{dataset.index}/mask_encode', data=dataset.masked_token)
 
+        if step % 1000 == 0:
+            print(f"write dataset into h5 format ... {step} / {len(datasets)}, {time.time() - s}")
+            s = time.time()
 
-class MakeBartMLMDataset:
-    def __init__(self, tokenizer, source_io, mask_ratio, repeat, comment_dir):
+    hf.create_dataset('data_num', data=len(datasets))
+    hf.create_dataset('max_length', data=max_length)
 
-        self.tokenizer = tokenizer
-        self.mask_ratio = mask_ratio
-        self.repeat = repeat
-
-        lines = self.read_source(source_io=source_io)
-
-        make_dataset(
-            split_lines=lines,
-            tokenizer=self.tokenizer,
-            repeat=repeat,
-            mask_ratio=mask_ratio,
-            path=comment_dir,
-        )
-
-    def read_source(self, source_io):
-        lines = []
-        s = time.time()
-        for line in source_io:
-            line = line.replace("\n", "")
-            lines.append(line)
-            if len(lines) % 1000 == 0:
-                print(f"read lines ... {len(lines)}, {time.time() - s}")
-                s = time.time()
-
-        print(f"read lines ... {len(lines)}, {time.time() - s}")
-        return lines
-
-    def get_split_index(self, total_length, n_proc, index):
-        split = int(total_length / n_proc)
-        start_index = index * split
-        end_index = (index + 1) * split
-        return start_index, end_index
-
+    print(f"write dataset all into h5 format")
 
 def main(args):
 
@@ -101,19 +89,19 @@ def main(args):
     dataset_path = comment_dir / "dataset.h5py"
 
     source_io = open(source_path, "r", encoding="utf-8")
+    input_args = read_txt_file(source_io, tokenizer,
+                               args.mask_ratio, args.repeat)
 
-    make_dataset = MakeBartMLMDataset(
-        tokenizer=tokenizer,
-        source_io=source_io,
-        mask_ratio=args.mask_ratio,
-        repeat=args.repeat,
-        comment_dir=comment_dir,
-    )
+    pool = multiprocessing.Pool(args.num_cpu)
+    datasets = pool.map(map_wrapper, input_args)
+
+    write_file(dataset_path, datasets)
 
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("--repeat", default=1, type=int)
+    parser.add_argument("--repeat", default=10, type=int)
+    parser.add_argument("--num_cpu", default=8, type=int)
     parser.add_argument("--mask_ratio", default=0.15, type=float)
     parser.add_argument("--tokenizer_path", default="tokenizers", type=str)
     args = parser.parse_args()
